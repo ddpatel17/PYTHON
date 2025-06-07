48d9245a15c26138aee5b1712e0d3249990f7c2c
ddpatel17 <parekhdiya7@gmail.com>
2025-06-07 19:54:06 +0530

    Add files via upload

--- 16_if.py
+++ 16_if.py
@@ -1,13 +1,5 @@
-# x = (int(input("enter your x ")))
-# y = (int(input("enter your y ")))
-# if x <10:
-#     x = 9
-
-
-lenght =(int(input("enter your lenght ")))
-width = (int(input("enter your width ")))
-if lenght <0:
-    lenght = -lenght 
-if width <0:
-    width = -width 
- 
+x = (int(input("enter your x ")))
+y = (int(input("enter your y ")))
+if x > y:  
+    print("first number is grater than second number ")
+# print("true")
