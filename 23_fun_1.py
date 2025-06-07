4b318fa1439bccd48006714598b2abbcbc4c039e
ddpatel17 <parekhdiya7@gmail.com>
2025-06-07 19:32:12 +0530

    Add files via upload

--- 23_fun_1.py
+++ 23_fun_1.py
@@ -1,26 +1,71 @@
-d ="diya patel"
-len(d) # default function
-print(d)
-# # user define function
+            # types of function
 
+#default function
+d ="diya patel"
+len(d) 
+print(d)
+
+# user define function
+ #with parameter with return 
 def addition(number):# with perameater 
     d =number + number
-    # return d # with perameater 
-    print("addition =", d) # output is addition = 2
+    return d # with return statement
+  
+   # calling function
 number = int(input("Enter your Number "))  
-print("addition =", number) # right 
-dd = addition(number)
-# print("addition =",dd) # return not passed then output is addition = None
+ans = addition(number)
+print("addition is =",ans) 
 
 
+  #without parameter without return 
+def greet(): # without parameter
+      print("hello,my name is diya ")# without return statement
+greet() # calling function
 
-# def nsquare(x,y = 3,2)# error wy?
-# def nsquare(x,y= 3): #here x is required and y is optional
-# def nsquare(y=2,x=4):   # for print("result is =",nsquare())
-#     return (x*x + 2*x*y + y*y) # (1*1+2*1*3+3*3)=1+6+9
-# result = nsquare(1) # x=1,y=3
-# print("result is =",result)
-# print("result is =",nsquare(2,4)) #  x=2,y=4(2*2+2*2*4+4*4)=4+16+16
-# print("result is =",nsquare(y=2,x=3)) #x=2,y=4(3*3+2*3*2+2*2)=9+12+4
-# print("result is =",nsquare())  #nsquare() missing 1 required positional argument: 'x'
+
+ # wihout parameter with return
+
+import datetime
+
+def get_current_datetime():
+    return datetime.datetime.now()
+
+# Calling the function
+current_time = get_current_datetime()
+print("Current Date and Time:", current_time)
+
+
+ # wih parameter without return
+
+month =int(input("Enter the month number"))
+
+def monthcount(month):
+    if month == 1:
+        print("january")
+    elif month == 2:
+        print("february")
+    elif month == 3:
+        print("march")
+    elif month == 4:
+        print("april")
+    elif month == 5:
+        print("may")
+    elif month == 6:
+        print("june")
+    elif month == 7:
+        print("july")
+    elif month == 8:
+        print("august")
+    elif month == 9:
+        print("september")
+    elif month == 10:
+        print("october")
+    elif month == 11:
+        print("november")
+    elif month == 12:
+        print("december")
+    else:
+        print("Invalid month number")
+monthcount(month)
+
 
