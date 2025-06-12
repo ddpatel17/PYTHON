3a7bd880d938495cd93accc5bf01815613438aa6
ddpatel17 <parekhdiya7@gmail.com>
2025-06-12 18:01:30 +0530

    math_module

--- 32_math_module.py
+++ 32_math_module.py
@@ -13,3 +13,17 @@ print(floor)#positive = input-(2.25)=2,
 
 
     #factorial- 
+
+
+import math
+x =float(input("enter the number "))
+prinr("ceil is =", math.ceil(x)) #ceil(x)return the smallest integer greater than or equal to x.
+prinr("floor is =", math.floor(x)) #floor(x)return the largest integer lessthan or equal to x.
+prinr("copysign is =", math.copysign(x,2)) #copysign(x,y)return x with the sign of y
+prinr("fabs is =", math.fabs(x)) # fabs(x)return theabsolute value of x
+prinr("factorial is =", math.factorial(x)) #factorial(x)return the factorial of x
+prinr("fmod is =", math.fmod(x,2)) # fmod(x,y)return the reminder when x is divided by y
+prinr("isfinite is =", math.isfinite(x)) # isfinite(x) return true if x is neither an infinity nor a nan (not a number)
+prinr("isinf is =", math.isinf(x)) # isinf(x)return true is x is a positive or negative infinity
+prinr("isnan is =", math.isnan(x)) # isnan(x)return true if x is a nan
+prinr("indexp is =", math.imdexp(x,i)) # return x*(2**i)
---
+++ 33_math_module.py
@@ -0,0 +1,15 @@
+import math
+x =float(input("enter the number "))
+
+print("modf is =", math.modf(x)) # modf(x)Returns the fractional and integer parts of x
+print("trunc is =", math.trunc(x))# trunc(x)Returns the truncated integer value of x
+print("exp is =", math.exp(x))# exp(x)Returns e**x
+print("expm1 is =", math.expm1(x))# expm1(x)Returns e**x - 1
+print("log is =", math.log(x[ ,base]))# log(x[, base])Returns the logarithm of x to the base (defaults to e)
+print("log1p is =", math.log1p(x))# log1p(x)returns the natural logarithm of 1+x
+print("log2 is =", math.log2(x))# log2(x)Returns the base-2 logarithm of x
+print("log10 is =", math.log10(x))# log10(x)Returns the base-10 logarithm of x
+print("pow is =", math.pow(x,2))# pow(x, y)Returns x raised to the power y
+print("sqrt is =", math.sqrt(x))# sqrt(x)Returns the square root of x
+print("acos is =", math.acos(x))# acos(x)Returns the arc cosine of x
+print("asin is =", math.asin(x))# asin(x)Returns the arc sine of x
