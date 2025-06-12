a42333506596800ca03182a49c556bc46242c06f
ddpatel17 <parekhdiya7@gmail.com>
2025-06-12 09:01:12 +0530

    MATH_MODULE

---
+++ 32_math_module.py
@@ -0,0 +1,15 @@
+    # ceil(x)-return the smallest integer greater than or equal to x.
+import math
+number =float(input("enter the number ")) 
+# ceil = math.ceil(number)
+# print(ceil) #positive = input-2.25=3,
+# negative = input-(-2.25)=-2
+
+
+    #floor(x)-return the largest integer lessthan or equal to x.
+floor = math.floor(number)
+print(floor)#positive = input-(2.25)=2,
+# negative = input-(-2.25)=-3
+
+
+    #factorial- 
