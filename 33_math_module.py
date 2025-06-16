f98aa260f857dc79a4048f7c03dc59860dd480a7
ddpatel17 <parekhdiya7@gmail.com>
2025-06-16 08:38:44 +0530

    MATH_MODULE

--- 33_math_module.py
+++ 33_math_module.py
@@ -1,13 +1,14 @@
 import math as m
 x =float(input("enter the number "))
 
-print("modf is =", m.modf(x)) # modf(x)Returns the fractional and integer parts of x
-print("trunc is =", m.trunc(x))# trunc(x)Returns the truncated integer value of x
-print("exp is =", m.exp(x))# exp(x)Returns e**x
-print("expm1 is =", m.expm1(x))# expm1(x)Returns e**x - 1
-# print("log is =", m.log(x[ ,base]))# log(x[, base])Returns the logarithm of x to the base (defaults to e)
-# print("log1p is =", m.log1p(x))# log1p(x)returns the natural logarithm of 1+x
-# print("log2 is =", m.log2(x))# log2(x)Returns the base-2 logarithm of x
-# print("log10 is =", m.log10(x))# log10(x)Returns the base-10 logarithm of x
-print("pow is =", m.pow(x,2))# pow(x, y)Returns x raised to the power y
-# print("sqrt is =", m.sqrt(x))# sqrt(x)Returns the square root of x
+print("ceil is =", m.ceil(x)) #ceil(x)return the smallest integer greater than or equal to x.
+print("floor is =", m.floor(x)) #floor(x)return the largest integer lessthan or equal to x.
+print("copysign is =", m.copysign(x,2)) #copysign(x,y)return x with the sign of y
+print("fabs is =", m.fabs(x)) # fabs(x)return theabsolute value of x
+# print("factorial is =", m.factorial(x)) #factorial(x)return the factorial of x
+print("fmod is =", m.fmod(x,2)) # fmod(x,y)return the reminder when x is divided by y
+print("isfinite is =", m.isfinite(x)) # isfinite(x) return true if x is neither an infinity nor a nan (not a number)
+print("isinf is =", m.isinf(x)) # isinf(x)return true is x is a positive or negative infinity
+print("isnan is =", m.isnan(x)) # isnan(x)return true if x is a nan
+
+# print("indexp is =", m.imdexp(x,)) # return x*(2**i)
