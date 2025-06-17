75a39c1c59a438732ca96f09800579b0b22105f0
ddpatel17 <parekhdiya7@gmail.com>
2025-06-17 18:39:23 +0530

    generate_otp

--- otp.py
+++ otp.py
@@ -1,12 +1,12 @@
 import random as r
 def generate_otp():
-    if radiant == 8:
+    if otp == 8:
         print((r.radiant(10,90)),(r.radiant(10,90)),(r.radiant(10,90)),(r.radiant(10,90)))
-    else radiant == 6:
+    else otp == 6:
          print((r.radiant(10,90)),(r.radiant(10,90)),(r.radiant(10,90)))
-    else radiant == 4:
+    else otp == 4:
          print((r.radiant(10,90)),(r.radiant(10,90)))     
-    else radiant == 2:
+    else otp == 2:
          print((r.radiant(10,90)))
     elif 0:
         
