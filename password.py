07dc99d2a89e938c55a6e4aa18fcfb53c6fa3c36
ddpatel17 <parekhdiya7@gmail.com>
2025-06-17 18:39:41 +0530

    generate_password

---
+++ password.py
@@ -0,0 +1,30 @@
+
+Alphabets=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+digits=['0','1','2','3','4','5','6','7','8','9']
+Special=['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+
+list = "--"
+print(s.join(Alphabets + digits + Special))
+
+
+import random
+import string
+
+def generate_password():
+    for i in range():
+        
+        if password == 12:
+            print(r.shuffle(str.lower(),str.lower(),str.lower()str.upper(),str.upper(),str.upper(),str.special(),str.special(),str.digit(),str.digit(),str.digit(),str.digit()))
+        else password == 8:
+            print(r.shuffle(str.lower(),str.lower(),str.upper(),str.upper(),str.Special(),str.digit(),str.digit(),str.digit()))    
+        else password == 4:
+            print(r.shuffle(str.special(),str.lower(),str.upper(),str.digit()))
+        elif 0:
+
+n = int(input("enter the number of password "))  
+output=(generate_password)
+print(n)     
+          
+
+
+
