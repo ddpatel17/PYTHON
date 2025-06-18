eeb5a6d854d1243d3a229fcb8fd3576dac0c9318
ddpatel17 <parekhdiya7@gmail.com>
2025-06-18 08:45:31 +0530

    generate_password

--- password.py
+++ password.py
@@ -1,23 +1,23 @@
 
 Alphabets=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-digits=['0','1','2','3','4','5','6','7','8','9']
+digit=['0','1','2','3','4','5','6','7','8','9']
 Special=['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
 
-list = "--"
-print(s.join(Alphabets + digits + Special))
+new = "--"
+print(str.join(Alphabets + digit + Special))
 
 
 import random
 import string
 
 def generate_password():
-    for i in range():
+    for i in range(length):
         
-        if password == 12:
-            print(r.shuffle(str.lower(),str.lower(),str.lower()str.upper(),str.upper(),str.upper(),str.special(),str.special(),str.digit(),str.digit(),str.digit(),str.digit()))
-        else password == 8:
+        if length == 12:
+            print(r.shuffle(str.lower(),str.lower(),str.lower(),str.upper(),str.upper(),str.upper(),str.special(),str.special(),str.digit(),str.digit(),str.digit(),str.digit()))
+        else length == 8:
             print(r.shuffle(str.lower(),str.lower(),str.upper(),str.upper(),str.Special(),str.digit(),str.digit(),str.digit()))    
-        else password == 4:
+        else length == 4:
             print(r.shuffle(str.special(),str.lower(),str.upper(),str.digit()))
         elif 0:
 
