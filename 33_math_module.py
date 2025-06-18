0ed30ddac31fcdbce7f143e72b7b2e3ffc774f75
ddpatel17 <parekhdiya7@gmail.com>
2025-06-18 09:03:46 +0530

    math_module

--- 33_math_module.py
+++ 33_math_module.py
@@ -3,12 +3,12 @@ x =float(input("enter the number "))
 
 print("ceil is =", m.ceil(x)) #ceil(x)return the smallest integer greater than or equal to x.
 print("floor is =", m.floor(x)) #floor(x)return the largest integer lessthan or equal to x.
-print("copysign is =", m.copysign(x,2)) #copysign(x,y)return x with the sign of y
+print("copysign is =", m.copysign(x,-2)) #copysign(x,y)return x with the sign of y
 print("fabs is =", m.fabs(x)) # fabs(x)return theabsolute value of x
-# print("factorial is =", m.factorial(x)) #factorial(x)return the factorial of x
+# print("factorial is =", m.factorial(int(x))) #factorial(x)return the factorial of x
 print("fmod is =", m.fmod(x,2)) # fmod(x,y)return the reminder when x is divided by y
-print("isfinite is =", m.isfinite(x)) # isfinite(x) return true if x is neither an infinity nor a nan (not a number)
-print("isinf is =", m.isinf(x)) # isinf(x)return true is x is a positive or negative infinity
-print("isnan is =", m.isnan(x)) # isnan(x)return true if x is a nan
-
-# print("indexp is =", m.imdexp(x,)) # return x*(2**i)
+n = int(x)
+print("isfinite is =", m.isfinite(n)) # isfinite(x) return true if x is neither an infinity nor a nan (not a number)
+print("isinf is =", m.isinf(n)) # isinf(x)return true is x is a positive or negative infinity
+print("isnan is =", m.isnan(n)) # isnan(x)return true if x is a nan
+print("idexp is =", m.ldexp(n,2)) # return x*(2**i)
