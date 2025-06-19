d8d29b783271ac483135a57f9bcf9b4537a7b26e
ddpatel17 <parekhdiya7@gmail.com>
2025-06-19 19:15:29 +0530

    str_methods_function

--- 39_string_function.py
+++ 39_string_function.py
@@ -31,7 +31,4 @@ alphabets = "a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7
 print(alphabets)   
 list2 = alphabets.split()
 print(list2) 
-# print(alphabets.split())
-print(str.split(sep="--"))
-name = "the easylearn Academy"
-print(name.join(sep="--"))
+print(alphabets.split(" "))
