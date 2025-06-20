9d964b056e9004fcce2e17b536ee27e889ff12b4
ddpatel17 <parekhdiya7@gmail.com>
2025-06-20 19:23:44 +0530

    password

--- password.py
+++ password.py
@@ -1,10 +1,10 @@
 
-Alphabets=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-digit=['0','1','2','3','4','5','6','7','8','9']
-Special=['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+# Alphabets=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# digit=['0','1','2','3','4','5','6','7','8','9']
+# Special=['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
 
-s = ","
-print(s.join(Alphabets + digit + Special))
+# s = ","
+# print(s.join(Alphabets + digit + Special))
 
 
 import random as r
@@ -32,23 +32,10 @@ def generate_password(length):
             for i in range(length - 4):
                 password += [r.choice(s.ascii_uppercase + s.ascii_lowercase + s.digits + s.punctuation)]
             r.shuffle(password)
-            # return ''.join(password)
-            n = int(input("enter a length of password: "))
-            ans = (generate_password)
-            print(ans)
-            print(generate_password(n))
-            # if ans:
-            #     print("Generated password:", ans)
-            # len = int(input("enter a length of password: "))
-            # password = p.generate_password(len)
-
+            generated_password = ' '.join(password)
             print("\n password is",generate_password)
             print("\n lenght of password is",len(password))
             save_password(generate_password)
-
-
-
-
 
 
 def save_password(password):
