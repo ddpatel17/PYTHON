fc815245bb29121f4886f166a3c418d83027e692
ddpatel17 <parekhdiya7@gmail.com>
2025-06-20 17:33:19 +0530

    datetime

---
+++ 41_datetime_1.py
@@ -0,0 +1,13 @@
+import datetime as dt
+# get current date & time 
+current_datetime = dt.datetime.now() # 'now' is method in datetime module
+print(current_datetime)
+
+# get current date 
+current_date = dt.date.today()
+print(current_date)  
+
+# get today yera,month,day with date object 
+print("current yera: ",currentdate.year)
+print("current month: ",currentdate.month)
+print("current day: ",currentdate.day)
---
+++ 42_datetime_2.py
@@ -0,0 +1,19 @@
+# datetime.date class 
+import datetime as dt
+birth_date = daatetime.date(2004,5,14)
+print(birth_date)
+      # or
+from datetime import date
+birth_date = date(2004,5,14)
+print(birth_date)
+
+
+# timestamp
+import datetime as dt
+ts = dt.now().timestamp()
+print("timestamp: ", ts) # float
+print("timestamp :", int(ts)) #int
+      # or
+from datetime import date
+ts = date.fromtimestamp(1750393592)
+print("date =", ts)
