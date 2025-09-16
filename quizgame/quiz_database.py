1cb339692da8b4307f0def442f5c578c2761af06
ddpatel17 <parekhdiya149@gmail.com>
2025-09-16 16:50:15 +0530

    project

---
+++ quizgame/quiz_database.py
@@ -0,0 +1,25 @@
+question_bank=[
+    {"text":"(1) (15*4)+(120/6)=? ","answer":"c"},
+    {"text":"(2) if 2x+3=13,then x=? ","answer":"b"},
+    {"text":"(3) A rectangle has length 12 cm and breadth 8 cm. Its area is:____ ","answer":"d" },
+    {"text":"(4) The average of 6, 8, 10, and 12 is:_____ ","answer":"b"},
+    {"text":"(5) A car travels 120 km in 2 hours. Its speed is:_____ ","answer":"c"},
+    {"text":"(6) The simple interest on ₹2000 at 5% per annum for 2 years is:____ ","answer":"b"},
+    {"text":"(7) If 2 : 5 = x : 20, then x = ? ","answer":"d"},
+    {"text":"(8) The value of sin 30∘ is:_____ ","answer":"b"},
+    {"text":"(9) if log10​ 1000 =x,then=? ","answer":"c"}
+]
+
+options=[["a. 70","b. 80","c. 90","d. 100"],
+         ["a. 3","b. 4","c. 5","d. 6"],
+         ["a. 80 cm²","b. 88 cm²","c. 90 cm²","d. 96 cm²"],
+         ["a. 8","b. 9","c. 10","d. 11"],
+         ["a. 50 km/h","b. 55 km/h","c. 60 km/h","d. 65 km/h"],
+         ["a. ₹150","b. ₹200","c. ₹250","d. ₹300"],
+         ["a. 6","b. 7","c. 8","d. 10"],
+         ["a. 0.25","b. 0.5","c. 0.75","d. 1"],
+         ["a. 1","b. 2","c. 3","d. 4"]
+]
+
+
+
